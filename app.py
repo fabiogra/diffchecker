434609f999c026f77311bed96c6da9aa07692670
Fabio Grasso <fabio.grasso@jobtome.com>
2023-12-07 23:52:27 +0100

    feat: use colored markdown

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 import streamlit as st
 import difflib
-from annotated_text import annotated_text
 
 def process_diff(diff):
     processed_diff = []
     for line in diff:
         if line.startswith('+'):
             # Additions in green
-            processed_diff.append((line, "", "#e6ffe6"))
+            processed_diff.append(f":green[{line}]")
         elif line.startswith('-'):
             # Deletions in red
-            processed_diff.append((line, "", "#ffe6e6"))
+            processed_diff.append(f":red[{line}]")
         else:
             # Unchanged lines
             processed_diff.append(line)
     return processed_diff
 
 # Set up the title and form for input
-st.title('Diff Tool')
-text1 = st.text_area("Text 1", height=250)
-text2 = st.text_area("Text 2", height=250)
+st.markdown('<center><h1>Diffchecker</h1></center>', unsafe_allow_html=True)
 
-# When the button is pressed, compute and display the diff
-if st.button('Compare'):
-    diff = difflib.ndiff(text1.splitlines(keepends=True), text2.splitlines(keepends=True))
-    processed_diff = process_diff(diff)
-    annotated_text(*processed_diff)
+cols = st.columns([1, 1])
+with cols[0]:
+    text1 = st.text_area("Text 1", height=250)
+with cols[1]:
+    text2 = st.text_area("Text 2", height=250)
+
+with st.columns([1, 3, 1])[1]:
+    if st.button('Compare', use_container_width=True):
+        diff = difflib.ndiff(text1.splitlines(keepends=True), text2.splitlines(keepends=True))
+        processed_diff = process_diff(diff)
+        with st.container(border=True):
+            st.markdown("<br>".join(processed_diff), unsafe_allow_html=True)
