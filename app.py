a92c90a6b33d886ba4de4276d825e12780c858b8
Fabio Grasso <fabio.grasso@jobtome.com>
2023-12-07 23:58:38 +0100

    feat: add title, icon, set layout wide and improve text

--- app.py
+++ app.py
@@ -15,17 +15,23 @@ def process_diff(diff):
             processed_diff.append(line)
     return processed_diff
 
-# Set up the title and form for input
+st.set_page_config(
+    page_title="Diffchecker",
+    page_icon="🔎",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    
+)
 st.markdown('<center><h1>Diffchecker</h1></center>', unsafe_allow_html=True)
 
 cols = st.columns([1, 1])
 with cols[0]:
-    text1 = st.text_area("Text 1", height=250)
+    text1 = st.text_area("Original text", height=250)
 with cols[1]:
-    text2 = st.text_area("Text 2", height=250)
+    text2 = st.text_area("Changed text", height=250)
 
 with st.columns([1, 3, 1])[1]:
-    if st.button('Compare', use_container_width=True):
+    if st.button('Find difference', use_container_width=True):
         diff = difflib.ndiff(text1.splitlines(keepends=True), text2.splitlines(keepends=True))
         processed_diff = process_diff(diff)
         with st.container(border=True):
