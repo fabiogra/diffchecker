4d11214e393d3520738ee5377abd31f8b8586c85
Fabio Grasso <fabio.grasso@jobtome.com>
2023-12-07 23:59:16 +0100

    style: lint

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import difflib
 
+
 def process_diff(diff):
     processed_diff = []
     for line in diff:
-        if line.startswith('+'):
+        if line.startswith("+"):
             # Additions in green
             processed_diff.append(f":green[{line}]")
-        elif line.startswith('-'):
+        elif line.startswith("-"):
             # Deletions in red
             processed_diff.append(f":red[{line}]")
         else:
@@ -15,14 +16,14 @@ def process_diff(diff):
             processed_diff.append(line)
     return processed_diff
 
+
 st.set_page_config(
     page_title="Diffchecker",
     page_icon="🔎",
     layout="wide",
     initial_sidebar_state="collapsed",
-    
 )
-st.markdown('<center><h1>Diffchecker</h1></center>', unsafe_allow_html=True)
+st.markdown("<center><h1>Diffchecker</h1></center>", unsafe_allow_html=True)
 
 cols = st.columns([1, 1])
 with cols[0]:
@@ -31,7 +32,7 @@ with cols[1]:
     text2 = st.text_area("Changed text", height=250)
 
 with st.columns([1, 3, 1])[1]:
-    if st.button('Find difference', use_container_width=True):
+    if st.button("Find difference", use_container_width=True):
         diff = difflib.ndiff(text1.splitlines(keepends=True), text2.splitlines(keepends=True))
         processed_diff = process_diff(diff)
         with st.container(border=True):
